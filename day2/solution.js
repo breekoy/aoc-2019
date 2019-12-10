a7a9085519337b55cd10f7551fbbdfce4c8335a6
breekoy <mcwall.breekoy@gmail.com>
2019-12-10 13:56:06 +0800

    add part2 solution

--- day2/solution.js
+++ day2/solution.js
@@ -1,6 +1,7 @@
 const intcodeValues = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,6,1,19,1,19,5,23,2,10,23,27,2,27,13,31,1,10,31,35,1,35,9,39,2,39,13,43,1,43,5,47,1,47,6,51,2,6,51,55,1,5,55,59,2,9,59,63,2,6,63,67,1,13,67,71,1,9,71,75,2,13,75,79,1,79,10,83,2,83,9,87,1,5,87,91,2,91,6,95,2,13,95,99,1,99,5,103,1,103,2,107,1,107,10,0,99,2,0,14,0]
 const overrideValues = [12, 2]
 
+//part 1 solution
 //intcodes will be your puzzle input, overrides will be the values that will be replacing intcode position 1 and 2
 const restoreGravityAssistProgram = (intcodes = [], overrides = []) => {
     if (overrides.length) {
@@ -25,4 +26,21 @@ const restoreGravityAssistProgram = (intcodes = [], overrides = []) => {
     return intcodes[0]
 }
 
-console.log(restoreGravityAssistProgram(intcodeValues, overrideValues))
+//part 2 solution
+//"memory" parameter will be the puzzle input
+const getNounVerb = (memory = []) => {
+    let output = 19690720
+    let result = 0
+
+    for (let noun = 0; noun < 100; noun++) {
+        for (let verb = 0; verb < 100; verb++) {
+            result = restoreGravityAssistProgram([...memory], [noun, verb])
+            if (result === output) {
+                return 100 * noun + verb
+            }
+        }
+    }
+}
+
+console.log(restoreGravityAssistProgram([...intcodeValues], [...overrideValues]), '<--- part 1 answer')
+console.log(getNounVerb([...intcodeValues]), '<--- part 2 answer')
