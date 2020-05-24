9458cf07b161d4208e95ab7e4664f03861210fed
breekoy <mcwall.breekoy@gmail.com>
2020-05-24 16:54:14 +0800

    wip

--- day3/solution.js
+++ day3/solution.js
@@ -1,21 +1,16 @@
-let paths = [
-    'R8,U5,L5,D3'.split(','),
-    'U7,R6,D4,L4'.split(',')
-]
+const wire1 = 'R75,D30,R83,U83,L12,D49,R71,U7,L72'.split(',')
+const wire2 = 'U62,R66,U55,R34,D71,R55,D58,R83'.split(',')
 
-let visitedCoordinates = []
-
-paths.forEach(path => {
+const getVisitedCoordinates = (wire = []) => { 
+    let visitedCoordinates = []
     let x = 0
     let y = 0
-    let pathCoordinates = []
 
-    path.forEach(direction => { 
-        
-        let moves = parseInt(direction.slice(1), 10)
-        
+    wire.forEach(path => {
+        let moves = parseInt(path.slice(1), 10)
+
         for (i = 0; i < moves; i++) { 
-            switch (direction.substr(0, 1)) { 
+            switch (path.substr(0, 1)) {
                 case 'R':
                     x++
                     break
@@ -28,18 +23,27 @@ paths.forEach(path => {
                 case 'D':
                     y--
                     break
+                default:
+                    break
             }
-            pathCoordinates.push({ x: x, y: y })
+            visitedCoordinates.push({ x: x, y: y })
         }
-
     })
-    visitedCoordinates.push(pathCoordinates)
-})
 
-//get intersections   
-// let intersections = visitedCoordinates.reduce((min, current) => min.filter(e => current.includes({x: e.x, y: e.y})))
+    return visitedCoordinates
+}
 
-//get minimum x coordinate
-// minXCoordinate = pathCoordinates.reduce((min, current) => current.x < min ? current.x : min, pathCoordinates[0].x)
- 
-// console.log(intersections)
+const getIntersections = (paths = []) => paths.filter((val, i, self)=>self.findIndex(k=>k.x==val.x && k.y == val.y)!=i)
+
+const getManhattanDistance = (intersections = []) => { 
+    let minimumDistance = []
+    minimumDistance.push(intersections.find(item => item.x === intersections.reduce((min, p) => p.x < min ? p.x : min, intersections[0].x)))
+
+    if (minimumDistance.length > 1) { 
+        minimumDistance[0] = minimumDistance.find(item => item.y === minimumDistance.reduce((min, p) => p.y < min ? p.y : min, minimumDistance[0].y))
+    }
+    
+    return minimumDistance
+}
+
+console.log(getManhattanDistance(getIntersections([...getVisitedCoordinates(wire1), ...getVisitedCoordinates(wire2)])))
